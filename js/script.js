7e37831f7b2b2560e2806abfc1b8c4b7a21f24a0
Marta Mendoza <2019084@icacademy.org>
2017-11-29 23:33:44 +0000

    fixed more in Javascript to have input to be in the console.log

--- js/script.js
+++ js/script.js
@@ -1,24 +1,21 @@
 //Class 1:
         // Fork and clone the repo and set up your project workspace and link files
         // Write HTML for your project
-        $("#1").click(function(){
-                $(".result").append("<p>Rock</p>");
-        });
-        $("#2").click(function(){
-           $(".result").append("<p>Paper</p>");
-        });
-        $("#4").click(function(){
-           $(".result").append("<p>Scissors</p>");     
-        });
+        
 //Class 2:  
         // Declare four global variables for intial state of the list of choices, the computer choice, the user choice, and the winner.  
         // Practice using console.log() and debugger to debug your code
         // Create your document ready function. 
         // Create a click hander for when the "Shoot!" button is clicked, takes in the user choice from the input field, 
         // Display the user choice to the output screen
+                $("#userchoice").click(function(){
+                        var a = $("#chose").val()
+                        console.log(a)
+                });
                 var choseWisely=["rock", "paper", "scissors"];
                 var answer=Math.floor(Math.random()*choseWisely.length);
                 console.log(choseWisely[answer]);
+                
 //Class 3:  
         // Randomly choose among 'rock', 'paper', or 'scissors' from the list of choices 
         // Display the computer choice to the output screen
